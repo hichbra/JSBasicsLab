b5a792a7b537e508001157a0da1126bd13ec17b6
hichbra <hicham.brahimi@laposte.net>
2016-10-04 15:16:49 +0200

    Toutes les fonctions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,1 @@
 console.log('Hello World!');
-
-var str = "imagepng"
-str.split('.');
-console.log(str.split('.'));
-str = str.split('.')
-
-if (str.length >= 2) {
-  console.log(str.pop());
-} else {
-  console.log('non');
-
-}
--- src/js_basics/index.js
+++ src/js_basics/index.js
@@ -3,9 +3,9 @@ export const isNumberEven = (i) => {
   return (i % 2 === 0);
 };
 
+// str will be a string, but it may not have a file extension.
+// Return the file extension (with no period) if it has one, otherwise false
 export const getFileExtension = (str) => {
-  // str will be a string, but it may not have a file extension.
-  // Return the file extension (with no period) if it has one, otherwise false
   str = str.split('.');
 
   if (str.length < 2) {
@@ -15,24 +15,63 @@ export const getFileExtension = (str) => {
   return str.pop();
 };
 
-
+// arr will be an array.
+// Return the longest string in the array
 export const longestString = (arr) => {
-  // arr will be an array.
-  // Return the longest string in the array
+  let long;
+  let tailleLong = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].length > tailleLong && typeof arr[i] === 'string') {
+      long = arr[i];
+      tailleLong = arr[i].length;
+    }
+  }
+
+  return long;
 };
 
+// str will be an string
+// Return a new string who's characters are in the opposite order to str's.
 export const reverseString = (str) => {
-  // str will be an string
-  // Return a new string who's characters are in the opposite order to str's.
+  return str.split('').reverse().join('');
 };
 
+// str will be an string
+// Return true if it is a palindrome and false otherwise.
+// It should be case insensitive and not consider space or punctuation.
 export const isPalindrome = (str) => {
-  // str will be an string
-  // Return true if it is a palindrome and false otherwise.
-  // It should be case insensitive and not consider space or punctuation.
+  str = str.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~ ()]/g, '');
+  let reverse = str.split('').reverse().join('');
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== reverse[i]) {
+      return false;
+    }
+  }
+
+  return true;
 };
 
+
+function sumArray(arr) {
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (!(arr[i] instanceof Array)) {
+      if (typeof (arr[i]) === 'number') {
+        sum += parseInt(arr[i], 10);
+      }
+    } else {
+      sum += sumArray(arr[i]);
+    }
+  }
+
+  return sum;
+}
+
+// arr will be an array, containing integers, strings and/or arrays like itself
+// Return the sum all the numbers you find, anywhere in the nest of arrays.
 export const nestedSum = (arr) => {
-  // arr will be an array, containing integers, strings and/or arrays like itself
-  // Return the sum all the numbers you find, anywhere in the nest of arrays.
+  return sumArray(arr);
 };
